eb9a9e238c9e53d2958043932c16ba1b88dbb567
DevArKaDiA <jfernandorojasc@gmail.com>
2023-11-17 02:15:13 -0500

    0.0.9

---
+++ main.py
@@ -0,0 +1,8 @@
+import sys
+from src.copypy.copypy import transcribe_video
+
+if __name__ == "__main__":
+    if len(sys.argv) < 3:
+        print("Usage: python main.py <video_path> <output_path>")
+        sys.exit(1)
+    transcribe_video(sys.argv[1], sys.argv[2])
